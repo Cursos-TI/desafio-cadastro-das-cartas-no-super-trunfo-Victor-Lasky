ed7fdfb183c9ebbb62a4f4fb566e45a27e7e092c
Victor-Lasky <victormoura99075719@gmail.com>
2025-01-20 23:47:52 +0000

    de

--- novo.c
+++ novo.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 int main() {
 // po==População
 // ar==Área
@@ -20,7 +22,7 @@ po_a01,po_a03,po_a04,po_b01,po_b02,po_b03,po_b04,po_c01,po_c02,po_c03,po_c04,po_
  
  
    printf("Digite o tamanho de sua população: \n");
-   scanf("%d", &po_a01);
+   scanf("%f", &po_a01);
  
    printf("Digite o tamanho de sua aréa em km: \n");
    scanf("%f", &ar_a01 );
@@ -37,7 +39,7 @@ po_a01,po_a03,po_a04,po_b01,po_b02,po_b03,po_b04,po_c01,po_c02,po_c03,po_c04,po_
  
  
    printf("Digite o tamanho de sua população: \n");
-   scanf("%d", &po_a02);
+   scanf("%f", &po_a02);
  
    printf("Digite o tamanho de sua aréa em km: \n");
    scanf("%f", &ar_a02);
@@ -53,7 +55,7 @@ printf("Estado A, Cidade 3\n");
  
  
    printf("Digite o tamanho de sua população: \n");
-   scanf("%d", &po_a03);
+   scanf("%f", &po_a03);
  
    printf("Digite o tamanho de sua aréa em km: \n");
    scanf("%f", &ar_a03);
@@ -70,7 +72,7 @@ printf("Estado A, Cidade 4\n");
  
  
    printf("Digite o tamanho de sua população: \n");
-   scanf("%d", &po_a04);
+   scanf("%f", &po_a04);
  
    printf("Digite o tamanho de sua aréa em km: \n");
    scanf("%f", &ar_a04);
@@ -86,7 +88,7 @@ printf("Estado B, Cidade 1\n");
  
  
    printf("Digite o tamanho de sua população: \n");
-   scanf("%d", &po_b01);
+   scanf("%f", &po_b01);
  
    printf("Digite o tamanho de sua aréa em km: \n");
    scanf("%f", &ar_b01);
@@ -103,7 +105,7 @@ printf("Estado B, Cidade 2\n");
  
  
    printf("Digite o tamanho de sua população: \n");
-   scanf("%d", &po_b02);
+   scanf("%f", &po_b02);
  
    printf("Digite o tamanho de sua aréa em km: \n");
    scanf("%f", &ar_b02);
@@ -121,7 +123,7 @@ printf("Estado B, Cidade 3\n");
  
  
    printf("Digite o tamanho de sua população: \n");
-   scanf("%d", &po_b03);
+   scanf("%f", &po_b03);
  
    printf("Digite o tamanho de sua aréa em km: \n");
    scanf("%f", &ar_b03);
@@ -137,7 +139,7 @@ printf("Estado B, Cidade 4\n");
  
  
    printf("Digite o tamanho de sua população: \n");
-   scanf("%d", &po_b04);
+   scanf("%f", &po_b04);
  
    printf("Digite o tamanho de sua aréa em km: \n");
    scanf("%f", &ar_b04);
@@ -153,7 +155,7 @@ printf("Estado C,Cidade 1\n");
  
  
    printf("Digite o tamanho de sua população: \n");
-   scanf("%d", &po_c01);
+   scanf("%f", &po_c01);
  
    printf("Digite o tamanho de sua aréa em km: \n");
    scanf("%f", &ar_c01);
@@ -169,7 +171,7 @@ printf("Estado C,Cidade 2\n");
  
  
    printf("Digite o tamanho de sua população: \n");
-   scanf("%d", &po_c02);
+   scanf("%f", &po_c02);
  
    printf("Digite o tamanho de sua aréa em km: \n");
    scanf("%f", &ar_c02);
@@ -186,7 +188,7 @@ printf("Estado C,Cidade 3\n");
  
  
    printf("Digite o tamanho de sua população: \n");
-   scanf("%d", &po_c03);
+   scanf("%f", &po_c03);
  
    printf("Digite o tamanho de sua aréa em km: \n");
    scanf("%f", &ar_c03);
@@ -203,7 +205,7 @@ printf("Estado C,Cidade 4\n");
  
  
    printf("Digite o tamanho de sua população: \n");
-   scanf("%d", &po_c04);
+   scanf("%f", &po_c04);
  
    printf("Digite o tamanho de sua aréa em km: \n");
    scanf("%f", &ar_c04);
@@ -221,7 +223,7 @@ printf("Estado D,Cidade 1\n");
  
  
    printf("Digite o tamanho de sua população: \n");
-   scanf("%d", &po_d01);
+   scanf("%f", &po_d01);
  
    printf("Digite o tamanho de sua aréa em km: \n");
    scanf("%f", &ar_d01);
@@ -237,7 +239,7 @@ printf("Estado D,Cidade 2\n");
  
  
    printf("Digite o tamanho de sua população: \n");
-   scanf("%d", &po_d02);
+   scanf("%f", &po_d02);
  
    printf("Digite o tamanho de sua aréa em km: \n");
    scanf("%f", &ar_d02);
@@ -255,7 +257,7 @@ printf("Estado D,Cidade 3\n");
  
  
    printf("Digite o tamanho de sua população: \n");
-   scanf("%d", &po_d03);
+   scanf("%f", &po_d03);
  
    printf("Digite o tamanho de sua aréa em km: \n");
    scanf("%f", &ar_d03);
@@ -271,7 +273,7 @@ printf("Estado D,Cidade 4\n");
  
  
    printf("Digite o tamanho de sua população: \n");
-   scanf("%d", &po_d04);
+   scanf("%f", &po_d04);
  
    printf("Digite o tamanho de sua aréa em km: \n");
    scanf("%f", &ar_d04);
@@ -287,7 +289,7 @@ printf("Estado E,Cidade 1\n");
  
  
    printf("Digite o tamanho de sua população: \n");
-   scanf("%d", &po_e01);
+   scanf("%f", &po_e01);
  
    printf("Digite o tamanho de sua aréa em km: \n");
    scanf("%f", &ar_e01);
@@ -304,7 +306,7 @@ printf("Estado E,Cidade 2\n");
  
  
    printf("Digite o tamanho de sua população: \n");
-   scanf("%d", &po_e02);
+   scanf("%f", &po_e02);
  
    printf("Digite o tamanho de sua aréa em km: \n");
    scanf("%f", &ar_e02);
@@ -321,7 +323,7 @@ printf("Estado E,Cidade 3\n");
  
  
    printf("Digite o tamanho de sua população: \n");
-   scanf("%d", &po_e03);
+   scanf("%f", &po_e03);
  
    printf("Digite o tamanho de sua aréa em km: \n");
    scanf("%f", &ar_e03);
@@ -337,7 +339,7 @@ printf("Estado E,Cidade 4\n");
  
  
    printf("Digite o tamanho de sua população: \n");
-   scanf("%d", &po_e04);
+   scanf("%f", &po_e04);
  
    printf("Digite o tamanho de sua aréa em km: \n");
    scanf("%f", &ar_e04);
@@ -353,7 +355,7 @@ printf("Estado F,Cidade 1\n");
  
  
    printf("Digite o tamanho de sua população: \n");
-   scanf("%d", &po_f01);
+   scanf("%f", &po_f01);
  
    printf("Digite o tamanho de sua aréa em km: \n");
    scanf("%f", &ar_f01);
@@ -370,7 +372,7 @@ printf("Estado F,Cidade 2\n");
  
  
    printf("Digite o tamanho de sua população: \n");
-   scanf("%d", &po_f02);
+   scanf("%f", &po_f02);
  
    printf("Digite o tamanho de sua aréa em km: \n");
    scanf("%f", &ar_f02);
@@ -387,7 +389,7 @@ printf("Estado F,Cidade 3\n");
  
  
    printf("Digite o tamanho de sua população: \n");
-   scanf("%d", &po_f03);
+   scanf("%f", &po_f03);
  
    printf("Digite o tamanho de sua aréa em km: \n");
    scanf("%f", &ar_f03);
@@ -404,7 +406,7 @@ printf("Estado F,Cidade 4\n");
  
  
    printf("Digite o tamanho de sua população: \n");
-   scanf("%d", &po_f04);
+   scanf("%f", &po_f04);
  
    printf("Digite o tamanho de sua aréa em km: \n");
    scanf("%f", &ar_f04);
@@ -420,7 +422,7 @@ printf("Estado F,Cidade 4\n");
  
  
    printf("Digite o tamanho de sua população: \n");
-   scanf("%d", &po_g01);
+   scanf("%f", &po_g01);
  
    printf("Digite o tamanho de sua aréa em km: \n");
    scanf("%f", &ar_g01);
@@ -438,7 +440,7 @@ printf("Estado G,Cidade 2\n");
  
  
    printf("Digite o tamanho de sua população: \n");
-   scanf("%d", &po_g02);
+   scanf("%f", &po_g02);
  
    printf("Digite o tamanho de sua aréa em km: \n");
    scanf("%f", &ar_g02);
@@ -455,7 +457,7 @@ printf("Estado G,Cidade 3\n");
  
  
    printf("Digite o tamanho de sua população: \n");
-   scanf("%d", &po_g03);
+   scanf("%f", &po_g03);
  
    printf("Digite o tamanho de sua aréa em km: \n");
    scanf("%f", &ar_g03);
@@ -472,7 +474,7 @@ printf("Estado G,Cidade 4\n");
  
  
    printf("Digite o tamanho de sua população: \n");
-   scanf("%d", &po_g04);
+   scanf("%f", &po_g04);
  
    printf("Digite o tamanho de sua aréa em km: \n");
    scanf("%f", &ar_g04);
@@ -488,7 +490,7 @@ printf("Estado H,Cidade 1\n");
  
  
    printf("Digite o tamanho de sua população: \n");
-   scanf("%d", &po_h01);
+   scanf("%f", &po_h01);
  
    printf("Digite o tamanho de sua aréa em km: \n");
    scanf("%f", &ar_h01);
@@ -504,7 +506,7 @@ printf("Estado H,Cidade 2\n");
  
  
    printf("Digite o tamanho de sua população: \n");
-   scanf("%d", &po_h02);
+   scanf("%f", &po_h02);
  
    printf("Digite o tamanho de sua aréa em km: \n");
    scanf("%f", &ar_h02);
@@ -522,7 +524,7 @@ printf("Estado H,Cidade 3\n");
  
  
    printf("Digite o tamanho de sua população: \n");
-   scanf("%d", &po_h03);
+   scanf("%f", &po_h03);
  
    printf("Digite o tamanho de sua aréa em km: \n");
    scanf("%f", &ar_h03);
@@ -538,7 +540,7 @@ printf("Estado H,Cidade 4\n");
  
  
    printf("Digite o tamanho de sua população: \n");
-   scanf("%d", &po_h04);
+   scanf("%f", &po_h04);
  
    printf("Digite o tamanho de sua aréa em km: \n");
    scanf("%f", &ar_h04);
@@ -550,38 +552,38 @@ printf("Estado H,Cidade 4\n");
    scanf("%d", &pt_h04);
 
 
- printf("Estado A Cidade 1,\n População: %2d\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_a01, ar_a01, pi_a01, pt_a01);
-printf("Estado A Cidade 2,\n População: %2d\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_a02, ar_a02, pi_a02, pt_a02);
-printf("Estado A Cidade 3,\n População: %2d\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_a03, ar_a03, pi_a03, pt_a03);
-printf("Estado A Cidade 4,\n População: %2d\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_a04, ar_a04, pi_a04, pt_a04);
-printf("Estado B Cidade 1,\n População: %2d\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_b01, ar_b01, pi_b01, pt_b01);
-printf("Estado B Cidade 2,\n População: %2d\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_b02, ar_b02, pi_b02, pt_b02);
-printf("Estado B Cidade 3,\n População: %2d\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_b03, ar_b03, pi_b03, pt_b03);
-printf("Estado B Cidade 4,\n População: %2d\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_b04, ar_b04, pi_b04, pt_b04);
-printf("Estado C Cidade 1,\n População: %2d\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_c01, ar_c01, pi_c01, pt_c01);
-printf("Estado C Cidade 2,\n População: %2d\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_c02, ar_c02, pi_c02, pt_c02);
-printf("Estado C Cidade 3,\n População: %2d\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_c03, ar_c03, pi_c03, pt_c03);
-printf("Estado C Cidade 4,\n População: %2d\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_c04, ar_c04, pi_c04, pt_c04);
-printf("Estado D Cidade 1,\n População: %2d\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_d01, ar_d01, pi_d01, pt_d01);
-printf("Estado D Cidade 2,\n População: %2d\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_d02, ar_d02, pi_d02, pt_d02);
-printf("Estado D Cidade 3,\n População: %2d\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_d03, ar_d03, pi_d03, pt_d03);
-printf("Estado D Cidade 4,\n População: %2d\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_d04, ar_d04, pi_d04, pt_d04);
-printf("Estado E Cidade 1,\n População: %2d\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_e01, ar_e01, pi_e01, pt_e01);
-printf("Estado E Cidade 2,\n População: %2d\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_e02, ar_e02, pi_e02, pt_e02);
-printf("Estado E Cidade 3,\n População: %2d\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_e03, ar_e03, pi_e03, pt_e03);
-printf("Estado E Cidade 4,\n População: %2d\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_e04, ar_e04, pi_e04, pt_e04);
-printf("Estado F Cidade 1,\n População: %2d\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_f01, ar_f01, pi_f01, pt_f01);
-printf("Estado F Cidade 2,\n População: %2d\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_f02, ar_f02, pi_f02, pt_f02);
-printf("Estado F Cidade 3,\n População: %2d\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_f03, ar_f03, pi_f03, pt_f03);
-printf("Estado F Cidade 4,\n População: %2d\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_f04, ar_f04, pi_f04, pt_f04);
-printf("Estado G Cidade 1,\n População: %2d\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_g01, ar_g01, pi_g01, pt_g01);
-printf("Estado G Cidade 2,\n População: %2d\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_g02, ar_g02, pi_g02, pt_g02);
-printf("Estado G Cidade 3,\n População: %2d\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_g03, ar_g03, pi_g03, pt_g03);
-printf("Estado G Cidade 4,\n População: %2d\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_g04, ar_g04, pi_g04, pt_g04);
-printf("Estado H Cidade 1,\n População: %2d\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_h01, ar_h01, pi_h01, pt_h01);
-printf("Estado H Cidade 2,\n População: %2d\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_h02, ar_h02, pi_h02, pt_h02);
-printf("Estado H Cidade 3,\n População: %2d\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_h03, ar_h03, pi_h03, pt_h03);
-printf("Estado H Cidade 4,\n População: %2d\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_h04, ar_h04, pi_h04, pt_h04);
+printf("Estado A Cidade 1,\n População: %2f\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_a01, ar_a01, pi_a01, pt_a01);
+printf("Estado A Cidade 2,\n População: %2f\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_a02, ar_a02, pi_a02, pt_a02);
+printf("Estado A Cidade 3,\n População: %2f\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_a03, ar_a03, pi_a03, pt_a03);
+printf("Estado A Cidade 4,\n População: %2f\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_a04, ar_a04, pi_a04, pt_a04);
+printf("Estado B Cidade 1,\n População: %2f\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_b01, ar_b01, pi_b01, pt_b01);
+printf("Estado B Cidade 2,\n População: %2f\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_b02, ar_b02, pi_b02, pt_b02);
+printf("Estado B Cidade 3,\n População: %2f\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_b03, ar_b03, pi_b03, pt_b03);
+printf("Estado B Cidade 4,\n População: %2f\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_b04, ar_b04, pi_b04, pt_b04);
+printf("Estado C Cidade 1,\n População: %2f\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_c01, ar_c01, pi_c01, pt_c01);
+printf("Estado C Cidade 2,\n População: %2f\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_c02, ar_c02, pi_c02, pt_c02);
+printf("Estado C Cidade 3,\n População: %2f\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_c03, ar_c03, pi_c03, pt_c03);
+printf("Estado C Cidade 4,\n População: %2f\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_c04, ar_c04, pi_c04, pt_c04);
+printf("Estado D Cidade 1,\n População: %2f\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_d01, ar_d01, pi_d01, pt_d01);
+printf("Estado D Cidade 2,\n População: %2f\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_d02, ar_d02, pi_d02, pt_d02);
+printf("Estado D Cidade 3,\n População: %2f\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_d03, ar_d03, pi_d03, pt_d03);
+printf("Estado D Cidade 4,\n População: %2f\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_d04, ar_d04, pi_d04, pt_d04);
+printf("Estado E Cidade 1,\n População: %2f\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_e01, ar_e01, pi_e01, pt_e01);
+printf("Estado E Cidade 2,\n População: %2f\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_e02, ar_e02, pi_e02, pt_e02);
+printf("Estado E Cidade 3,\n População: %2f\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_e03, ar_e03, pi_e03, pt_e03);
+printf("Estado E Cidade 4,\n População: %2f\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_e04, ar_e04, pi_e04, pt_e04);
+printf("Estado F Cidade 1,\n População: %2f\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_f01, ar_f01, pi_f01, pt_f01);
+printf("Estado F Cidade 2,\n População: %2f\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_f02, ar_f02, pi_f02, pt_f02);
+printf("Estado F Cidade 3,\n População: %2f\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_f03, ar_f03, pi_f03, pt_f03);
+printf("Estado F Cidade 4,\n População: %2f\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_f04, ar_f04, pi_f04, pt_f04);
+printf("Estado G Cidade 1,\n População: %2f\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_g01, ar_g01, pi_g01, pt_g01);
+printf("Estado G Cidade 2,\n População: %2f\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_g02, ar_g02, pi_g02, pt_g02);
+printf("Estado G Cidade 3,\n População: %2f\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_g03, ar_g03, pi_g03, pt_g03);
+printf("Estado G Cidade 4,\n População: %2f\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_g04, ar_g04, pi_g04, pt_g04);
+printf("Estado H Cidade 1,\n População: %2f\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_h01, ar_h01, pi_h01, pt_h01);
+printf("Estado H Cidade 2,\n População: %2f\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_h02, ar_h02, pi_h02, pt_h02);
+printf("Estado H Cidade 3,\n População: %2f\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_h03, ar_h03, pi_h03, pt_h03);
+printf("Estado H Cidade 4,\n População: %2f\n Área: %2f\n PIB: %2f\n Número de pontos turísticos: %2d\n", po_h04, ar_h04, pi_h04, pt_h04);
 
 
  return 0;
