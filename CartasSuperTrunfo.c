75c09901532cb24747923a2d1ccd48f5752d24a4
Victor-Lasky <victormoura99075719@gmail.com>
2025-02-08 02:32:21 +0000

    attz

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,5 +1,330 @@
 #include <stdio.h>
 
+struct A1 {
+
+int po_a01;
+float ar_a01;
+float pi_a01;
+int pt_a01;
+float dp_a01;
+
+};
+
+struct A2 {
+
+int po_a02;
+float ar_a02;
+float pi_a02;
+int pt_a02;
+float dp_a02;
+
+};
+
+struct A3 {
+
+int po_a03;
+float ar_a03;
+float pi_a03;
+int pt_a03;
+float dp_a03;
+
+};
+
+struct A4 {
+
+int po_a04;
+float ar_a04;
+float pi_a04;
+int pt_a04;
+float dp_a04;
+
+};
+
+struct B1 {
+
+int po_b01;
+float ar_b01;
+float pi_b01;
+int pt_b01;
+float dp_b01;
+
+};
+
+struct B2 {
+
+int po_b02;
+float ar_b02;
+float pi_b02;
+int pt_b02;
+float dp_b02;
+
+};
+
+struct B3 {
+
+int po_b03;
+float ar_b03;
+float pi_b03;
+int pt_b03;
+float dp_b03;
+
+};
+
+struct B4 {
+
+int po_b04;
+float ar_b04;
+float pi_b04;
+int pt_b04;
+float dp_b04;
+
+};
+
+struct C1 {
+
+int po_c01;
+float ar_c01;
+float pi_c01;
+int pt_c01;
+float dp_c01;
+
+};
+
+struct C2 {
+
+int po_c02;
+float ar_c02;
+float pi_c02;
+int pt_c02;
+float dp_c02;
+
+};
+
+struct C3 {
+
+int po_c03;
+float ar_c03;
+float pi_c03;
+int pt_c03;
+float dp_c03;
+
+};
+
+struct C4 {
+
+int po_c04;
+float ar_c04;
+float pi_c04;
+int pt_c04;
+float dp_c04;
+
+};
+
+struct D1 {
+
+int po_d01;
+float ar_d01;
+float pi_d01;
+int pt_d01;
+float dp_d01;
+
+};
+
+struct D2 {
+
+int po_d02;
+float ar_d02;
+float pi_d02;
+int pt_d02;
+float dp_d02;
+
+};
+
+struct D3 {
+
+int po_d03;
+float ar_d03;
+float pi_d03;
+int pt_d03;
+float dp_d03;
+
+};
+
+struct D4 {
+
+int po_d04;
+float ar_d04;
+float pi_d04;
+int pt_d04;
+float dp_d04;
+
+};
+
+struct E1 {
+
+int po_e01;
+float ar_e01;
+float pi_e01;
+int pt_e01;
+float dp_e01;
+
+};
+
+struct E2 {
+
+int po_e02;
+float ar_e02;
+float pi_e02;
+int pt_e02;
+float dp_e02;
+
+};
+
+struct E3 {
+
+int po_e03;
+float ar_e03;
+float pi_e03;
+int pt_e03;
+float dp_e03;
+
+};
+
+struct E4 {
+
+int po_e04;
+float ar_e04;
+float pi_e04;
+int pt_e04;
+float dp_e04;
+
+};
+
+struct F1 {
+
+int po_f01;
+float ar_f01;
+float pi_f01;
+int pt_f01;
+float dp_f01;
+
+};
+
+struct F2 {
+
+int po_f02;
+float ar_f02;
+float pi_f02;
+int pt_f02;
+float dp_f02;
+
+};
+
+struct F3 {
+
+int po_f03;
+float ar_f03;
+float pi_f03;
+int pt_f03;
+float dp_f03;
+
+};
+
+struct F4 {
+
+int po_f04;
+float ar_f04;
+float pi_f04;
+int pt_f04;
+float dp_f04;
+
+};
+
+struct G1 {
+
+int po_g01;
+float ar_g01;
+float pi_g01;
+int pt_g01;
+float dp_g01;
+
+};
+
+struct G2 {
+
+int po_g02;
+float ar_g02;
+float pi_g02;
+int pt_g02;
+float dp_g02;
+
+};
+
+struct G3 {
+
+int po_g03;
+float ar_g03;
+float pi_g03;
+int pt_g03;
+float dp_g03;;
+
+};
+
+struct G4 {
+
+int po_g04;
+float ar_g04;
+float pi_g04;
+int pt_g04;
+float dp_g04;
+
+};
+
+struct H1 {
+
+int po_h01;
+float ar_h01;
+float pi_h01;
+int pt_h01;
+float dp_h01;
+
+};
+
+struct H2 {
+
+int po_h02;
+float ar_h02;
+float pi_h02;
+int pt_h02;
+float dp_h02;
+
+};
+
+struct H1 {
+
+int po_h03;
+float ar_h03;
+float pi_h03;
+int pt_h03;
+float dp_h03;
+
+};
+
+struct H4 {
+
+int po_h04;
+float ar_h04;
+float pi_h04;
+int pt_h04;
+float dp_h04;
+
+};
+
+
+
+
+
+
 int main() {
 // po==População
 // ar==Área
@@ -7,14 +332,13 @@ int main() {
 //pt==Número de pontos turísticos**
 //dp==Densidade Populacional
 //pic==Pib per Capita 
-  int pt_a01,t_a02,pt_a03,pt_a04,pt_b01,pt_b02,pt_b03,pt_b04,pt_c01,pt_c02,pt_c03,pt_c04,t_d01,pt_a02,pt_d03,pt_d04,pt_e01,pt_e02,pt_e03,pt_e04,
+  
+int pt_a01,t_a02,pt_a03,pt_a04,pt_b01,pt_b02,pt_b03,pt_b04,pt_c01,pt_c02,pt_c03,pt_c04,t_d01,pt_a02,pt_d03,pt_d04,pt_e01,pt_e02,pt_e03,pt_e04,
   pt_f01,pt_f02,pt_f03,pt_f04,pt_g01,pt_g02,pt_g03,pt_g04,pt_h01,pt_h02,pt_h03,pt_h0,po_a01,po_a03,po_a04,po_b01,po_b02,po_b03,po_b04,po_c01,po_c02,po_c03,
   po_c04,po_d02,po_d03,po_d04,po_e01,po_e02,po_e03,po_e04,po_f01,po_f02,po_f03,po_f04,po_g01,po_g02,po_g03,po_g04,po_h01,po_h02,po_h03,po_h04,po_a02;
   float ar_a01,ar_a03,ar_a04,ar_b01,ar_b02,ar_b03,ar_b04,ar_c01,ar_c02,ar_c03,ar_c04,ar_d02,ar_d03,ar_d04,ar_e01,ar_e02,ar_e03,ar_e04,ar_f01,ar_f02,ar_f03,ar_f04,ar_g01,ar_g02,ar_g03,ar_g04,ar_h01,ar_h02,ar_h03,ar_h04,pt_d02,
  pi_a01,pi_a03,pi_a04,pi_b01,pi_b02,pi_b03,pi_b04,pi_c01,pi_c02,pi_c03,pi_c04,pi_d02,pi_d03,pi_d04,pi_e01,pi_e02,pi_e03,pi_e04,pi_f01,pi_f02,pi_f03,pi_f04,pi_g01,pi_g02,pi_g03,pi_g04,pi_h01,pi_h02,pi_h03,pi_h04,po_d01,ar_d01,pi_d01,pt_d01,
  ar_a02,pi_a02,pt_h04;
-
-
 
 
    printf("Cartas Super Trunfo \n");
@@ -680,5 +1004,113 @@ printf("Estado H, Cidade 3,\n População: %2d\n Área: %.2f\n PIB: %.2f\n Núme
 printf("Estado H, Cidade 4,\n População: %2d\n Área: %.2f\n PIB: %.2f\n Número de pontos turísticos: %2d\n Densidade: %.2f\n PIB per Capita %.2f\n\n",po_h04,ar_h04,pi_h04,pt_h04,dp_h04,pic_h04);
 
 
+printf("Suas cartas foram cadastradas.\n Cada uma delas tem um código, que é denominado pelo 'Estado' e 'Cidade' como você acompanhou no início do cadastro.");
+printf("Carregando....");
+printf("A combinação da letra do Estado e o número da Cidade define o código, por exemplo(A1,A2,B3,B4,E2,H3).");
+
+
+
+//Funcão para imprimir valores
+
+void imprimirA1(struct A1); {
+   printf("Estado A, Cidade 1,\n População: %2d\n Área: %.2f\n PIB: %.2f\n Número de pontos turísticos: %2d\n Densidade: %.2f\n PIB per Capita %.2f\n\n",po_a01,ar_a01,pi_a01,pt_a01,dp_a01,pic_a01);
+}
+void imprimirA2(struct A2); {
+   printf("Estado A, Cidade 2,\n População: %2d\n Área: %.2f\n PIB: %.2f\n Número de pontos turísticos: %2d\n Densidade: %.2f\n PIB per Capita %.2f\n\n",po_a02,ar_a02,pi_a02,pt_a02, dp_a02,pic_a02);
+}
+void imprimirA3(struct A3); {
+   printf("Estado A, Cidade 3,\n População: %2d\n Área: %.2f\n PIB: %.2f\n Número de pontos turísticos: %2d\n Densidade: %.2f\n PIB per Capita %.2f\n\n",po_a03,ar_a03,pi_a03,pt_a03,dp_a03,pic_a03);
+}
+void imprimirA4(struct A4);{
+   printf("Estado A, Cidade 4,\n População: %2d\n Área: %.2f\n PIB: %.2f\n Número de pontos turísticos: %2d\n Densidade: %.2f\n PIB per Capita %.2f\n\n",po_a04,ar_a04,pi_a04,pt_a04,dp_a04,pic_a04);
+}
+void imprimirB1(struct B1);{
+   printf("Estado B, Cidade 1,\n População: %2d\n Área: %.2f\n PIB: %.2f\n Número de pontos turísticos: %2d\n Densidade: %.2f\n PIB per Capita %.2f\n\n",po_b01,ar_b01,pi_b01,pt_b01,dp_b01,pic_b01);
+}
+void imprimirB2(struct B2);{
+   printf("Estado B, Cidade 2,\n População: %2d\n Área: %.2f\n PIB: %.2f\n Número de pontos turísticos: %2d\n Densidade: %.2f\n PIB per Capita %.2f\n\n",po_b02,ar_b02,pi_b02,pt_b02,dp_b02,pic_b02);
+}
+void imprimirB3(struct B3);{
+   printf("Estado B, Cidade 3,\n População: %2d\n Área: %.2f\n PIB: %.2f\n Número de pontos turísticos: %2d\n Densidade: %.2f\n PIB per Capita %.2f\n\n",po_b03,ar_b03,pi_b03,pt_b03,dp_b03,pic_b03);
+}
+void imprimirB4(struct B4);{
+   printf("Estado B, Cidade 4,\n População: %2d\n Área: %.2f\n PIB: %.2f\n Número de pontos turísticos: %2d\n Densidade: %.2f\n PIB per Capita %.2f\n\n",po_b04,ar_b04,pi_b04,pt_b04,dp_b04,pic_b04);
+}
+void imprimirC1(struct C1);{
+   printf("Estado C, Cidade 1,\n População: %2d\n Área: %.2f\n PIB: %.2f\n Número de pontos turísticos: %2d\n Densidade: %.2f\n PIB per Capita %.2f\n\n",po_c01,ar_c01,pi_c01,pt_c01,dp_c01,pic_c01);
+}
+void imprimirC2(struct C2);{
+   printf("Estado C, Cidade 2,\n População: %2d\n Área: %.2f\n PIB: %.2f\n Número de pontos turísticos: %2d\n Densidade: %.2f\n PIB per Capita %.2f\n\n",po_c02,ar_c02,pi_c02,pt_c02,dp_c02,pic_c02);
+}
+void imprimirC3(struct C3);{
+   printf("Estado C, Cidade 3,\n População: %2d\n Área: %.2f\n PIB: %.2f\n Número de pontos turísticos: %2d\n Densidade: %.2f\n PIB per Capita %.2f\n\n",po_c03,ar_c03,pi_c03,pt_c03,dp_c03,pic_c03);
+}
+void imprimirC4(struct C4);{
+   printf("Estado C, Cidade 4,\n População: %2d\n Área: %.2f\n PIB: %.2f\n Número de pontos turísticos: %2d\n Densidade: %.2f\n PIB per Capita %.2f\n\n",po_c04,ar_c04,pi_c04,pt_c04,dp_c04,pic_c04);
+}
+void imprimirD1(struct D1);{
+   printf("Estado D, Cidade 1,\n População: %2d\n Área: %.2f\n PIB: %.2f\n Número de pontos turísticos: %2d\n Densidade: %.2f\n PIB per Capita %.2f\n\n",po_d01,ar_d01,pi_d01,pt_d01,dp_d01,pic_d01);
+}
+void imprimirD2(struct D2);{
+   printf("Estado D, Cidade 2,\n População: %2d\n Área: %.2f\n PIB: %.2f\n Número de pontos turísticos: %2d\n Densidade: %.2f\n PIB per Capita %.2f\n\n",po_d02,ar_d02,pi_d02,pt_d02,dp_d02,pic_d02);
+}
+void imprimirD3(struct D3);{
+   printf("Estado D, Cidade 3,\n População: %2d\n Área: %.2f\n PIB: %.2f\n Número de pontos turísticos: %2d\n Densidade: %.2f\n PIB per Capita %.2f\n\n",po_d03,ar_d03,pi_d03,pt_d03,dp_d03,pic_d03);
+}
+void imprimirD4(struct D4);{
+   printf("Estado D, Cidade 4,\n População: %2d\n Área: %.2f\n PIB: %.2f\n Número de pontos turísticos: %2d\n Densidade: %.2f\n PIB per Capita %.2f\n\n",po_d04,ar_d04,pi_d04,pt_d04,dp_d04,pic_d04);
+}
+void imprimirE1(struct E1);{ 
+   printf("Estado E, Cidade 1,\n População: %2d\n Área: %.2f\n PIB: %.2f\n Número de pontos turísticos: %2d\n Densidade: %.2f\n PIB per Capita %.2f\n\n",po_e01,ar_e01,pi_e01,pt_e01,dp_e01,pic_e01);
+}
+void imprimirE2(struct E2);{
+   printf("Estado E, Cidade 2,\n População: %2d\n Área: %.2f\n PIB: %.2f\n Número de pontos turísticos: %2d\n Densidade: %.2f\n PIB per Capita %.2f\n\n",po_e02,ar_e02,pi_e02,pt_e02,dp_e02,pic_e02);
+}
+void imprimirE3(struct E3);{
+   printf("Estado E, Cidade 3,\n População: %2d\n Área: %.2f\n PIB: %.2f\n Número de pontos turísticos: %2d\n Densidade: %.2f\n PIB per Capita %.2f\n\n",po_e03,ar_e03,pi_e03,pt_e03,dp_e03,pic_e03);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int comparar1,comparar2;
+
+printf("Escolha a primeira carta para comparar:\n\n");
+scanf("%f", &comparar1);
+ 
+printf("Escolha a segunda carta para comparar:\n\n");
+scanf("%f", &comparar2);
+ 
+if (comparar1,comparar2 != "A1,A2,A3,A4,B1,B2,B3,B4,C1,C2,C3,C4,D1,D2,D3,D4,E1,E2,E3,E4,F1,F2,F3,F4,G1,G2,G3,G4,H1,H2,H3,H4") {
+   printf("Escolha inválida.Escolha entre os códigos a seguir: A1,A2,A3,A4,B1,B2,B3,B4,C1,C2,C3,C4,D1,D2,D3,D4,E1,E2,E3,E4,F1,F2,F3,F4,G1,G2,G3,G4,H1,H2,H3,H4");
+} else {
+   printf 
+}
+ 
+
+
+
+ 
+ 
  return 0;
+
+
 }
